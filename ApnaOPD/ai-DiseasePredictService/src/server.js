11f116d67f8490138e880ecfb4142de4ce9b871c
fareedalam <fareedaalam.1512@gmail.com>
2025-08-17 12:46:18 +0530

    Fix Container isssue

--- ApnaOPD/ai-DiseasePredictService/src/server.js
+++ ApnaOPD/ai-DiseasePredictService/src/server.js
@@ -1,14 +1,18 @@
-const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const diseaseRoutes = require("./routes/diseaseRoutes");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); // allow all origins
-// OR app.use(cors({ origin: "http://localhost:5173" })); // restrict to React app
+//app.use(cors()); // allow all origins
+
+app.use(cors({ origin: "*" }));  // allow all
+//app.options("*", cors());        // handle preflight
+
+//app.use(cors({ origin: "http://localhost:5173" })); // restrict to React app
 
 app.use(express.json());
 
